f1e7a27749a979e0c4c874c07ba6052776af17fb
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2024-12-20 12:11:15 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import RecomendedArticles from "@/components/recomended/RecomendedArticles";
 import Space from "@/components/space/Space";
 import Tenants from "@/components/tenants/Tenants";
 import Image from "next/image";
+import { FaIcons, FaWhatsapp } from "react-icons/fa";
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
@@ -21,6 +22,16 @@ export default function Home() {
       <Tenants />
       <Gallery />
       <RecomendedArticles />
+      <a
+        href="https://wa.me/+621181110556
+"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-8 right-8 z-50 bg-white text-textDark p-4 rounded-full shadow-lg transition duration-300 flex items-center gap-2 md:gap-3"
+      >
+        <FaWhatsapp className="h-6 w-6" />
+        <span className="hidden md:inline font-medium">Hubungi Kami</span>
+      </a>
     </>
   );
 }
