ba03b5c412dcdd5db5340755fc8e9295c001e82d
Ervan Ade Wijaya <89782667+ervanade@users.noreply.github.com>
2025-03-24 14:35:58 +0700

    fix link desc

--- components/recomended/RecomendedArticles.jsx
+++ components/recomended/RecomendedArticles.jsx
@@ -97,6 +97,8 @@ const RecomendedArticles = ({ data }) => {
                           href={`/articles/${article.slug}?lang=${lang}`}
                           className="align-middle select-none font-sans font-bold text-center uppercase transition-all disabled:opacity-50 disabled:shadow-none disabled:pointer-events-none text-xs py-3 px-6 rounded-full bg-[#303638] text-white shadow-md shadow-gray-900/10 hover:shadow-lg hover:shadow-gray-900/20 focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none"
                           type="button"
+                          aria-label={`Baca lebih lanjut tentang ${lang === "en" ? article.title_en : article.title_id}`}
+                title={`Baca lebih lanjut tentang ${lang === "en" ? article.title_en : article.title_id}`}
                         >
                           Read More
                         </Link>
